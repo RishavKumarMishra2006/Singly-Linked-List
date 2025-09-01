703b9c725e8d005ed641cd414ea90f95b470b717
Rishav_Kumar_Mishra <rishavkumarmishra001@gmail.com>
2025-09-01 12:20:06 +0530

    Create Remove loop.c

---
+++ Remove loop.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->next = NULL;
+    return node;
+}
+bool detectLoop(struct Node* head) {
+    struct Node* slow = head, *fast = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) return true;
+    }
+    return false;
+}
