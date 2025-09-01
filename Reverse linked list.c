37c500da12fb5afec1d94f4a908a46936926b6b7
Rishav_Kumar_Mishra <rishavkumarmishra001@gmail.com>
2025-09-01 12:22:50 +0530

    Create Reverse linked list.c

---
+++ Reverse linked list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->next = NULL;
+    return node;
+}
+struct Node* reverseList(struct Node* head) {
+    struct Node* prev = NULL, *curr = head, *next = NULL;
+    while (curr) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
